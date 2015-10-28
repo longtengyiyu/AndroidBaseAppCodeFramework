ce841fc03af22cb96b8c5a6fb4ea9404150a039f
Oscar <longtengyiyu@gmail.com>
2015-10-28 17:17:53 +0800

    change:[web js interface]

--- app/src/main/java/in/guanjia/demo/web/WebAppInterface.java
+++ app/src/main/java/in/guanjia/demo/web/WebAppInterface.java
@@ -1,13 +1,16 @@
 package in.guanjia.demo.web;
 
+import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.webkit.JavascriptInterface;
 
+import in.guanjia.demo.R;
 import in.guanjia.demo.util.LogUtils;
 import in.guanjia.demo.util.ToastUtils;
 
 /**
- * Description:
+ * Description:web js 和 app交互
  * Author:    Oscar
  * Version    V1.0
  * Date:      2015/10/22
@@ -21,15 +24,21 @@ public class WebAppInterface {
 
     private static final String TAG = "WebAppInterface";
 
+    private Context mContext;
+
+    public WebAppInterface(Context context) {
+        this.mContext = context;
+    }
+
     /**
      *
      * @param view
      * @param toast
      */
     @JavascriptInterface
-    public void showToast(View view, String toast) {
+    public void showToast(String toast) {
         LogUtils.d(TAG, "toast = " + toast);
-        ToastUtils.getInstance().showInfo(view, toast);
+        ToastUtils.getInstance().showInfo(((Activity)mContext).findViewById(R.id.root_view), toast);
     }
 
 }
